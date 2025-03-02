8d11e332d29d21ce3c1e87e3257189f1920baa74
Godawela <godawelaus.21@uom.lk>
2025-03-02 21:20:09 +0530

    http

--- src/main/java/com/librarySystem/demo/Services/SecurityConfig.java
+++ src/main/java/com/librarySystem/demo/Services/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
@@ -15,11 +16,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/api/auth/**").permitAll()
-            .anyRequest().authenticated()
-            .and().httpBasic();
+        http.csrf(csrf -> csrf.disable())
+                .authorizeRequests(requests -> requests
+                        .antMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()).httpBasic(withDefaults());
         
         return http.build();
     }
