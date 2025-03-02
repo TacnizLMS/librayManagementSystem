74b3e2988f5f0a60fa2673de5a8ea211af537e4c
Godawela <godawelaus.21@uom.lk>
2025-03-02 21:19:02 +0530

    auth

--- src/main/java/com/librarySystem/demo/Models/User.java
+++ src/main/java/com/librarySystem/demo/Models/User.java
@@ -2,14 +2,18 @@ package com.librarySystem.demo.Models;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 @Document(collection = "users")
 public class User {
     @Id
     private String id;
-    private String name;
+    private String username;
     private String email;
+    private String password;
+    private Set<String> roles;
+    private String resetToken;
 }
--- src/main/java/com/librarySystem/demo/Repository/UserRepository.java
+++ src/main/java/com/librarySystem/demo/Repository/UserRepository.java
@@ -4,5 +4,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.librarySystem.demo.Models.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByResetToken(String token);
 }
